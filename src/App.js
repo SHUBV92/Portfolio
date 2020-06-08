e319c36f063e6ef0667b1e349742ce406284ffc4
your Shubinder Virk <shubinder92@gmail.com>
2020-06-09 00:34:04 +0100

    Added Contact Form

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import Contact from "./Containers/Contact/Contact";
 
 function App() {
   return (
@@ -46,6 +47,8 @@ function App() {
             <Route path="/">
               <ParallaxDesign />
             </Route>
+
+            <Contact />
 
           </ParallaxProvider>
         </Switch>
--- src/Containers/Contact/Contact.jsx
+++ src/Containers/Contact/Contact.jsx
@@ -2,7 +2,21 @@ import React from "react";
 
 const Contact = () => { 
 return (
-    Contact Me
+    <div>
+        <h1>Your Email address</h1>
+        <form>
+        <input 
+        type="email"
+        placeholder="Enter your email address"/>
+        <h1>Your Message</h1>
+        <textarea>Enter Your Epic Message</textarea>
+        <button>Submit</button>
+        </form>
+        <button>
+            <a href="linkedin.com/in/shubinder-virk-40508982/">Linkedin</a>
+        </button>
+
+    </div>
 )
 }
 
